4b4224ba272f92a296891f49fced5a78e026cbf6
JefferyChiang <36859596+JefferyChiang@users.noreply.github.com>
2022-08-10 21:26:06 +0800

    Add files via upload

---
+++ 加速度測試/加速度測試.py
@@ -0,0 +1,258 @@
+from cProfile import label
+import os
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from scipy import integrate
+import os
+import sys
+import numpy as np 
+import math
+import matplotlib.pyplot as plt
+from scipy.fftpack import fft
+from scipy import signal
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+# 1. 先校正加速規
+# 2. 再利用靜止加速度找出靜止加速度之平均與最大最小門檻值
+# 3. 濾波 -> 資料中心化 -> 門檻值 -> 積分
+
+minx = -0.055229175
+maxx = 0.474022503
+miny = -0.066014847
+maxy = 0.475204153
+minz = -0.047074322
+maxz = 0.488912852
+mapmin = -100
+mapmax = 100
+
+fs=50 #512
+cutoff=10 #10
+numtaps=155 #155
+
+def calibrate(data, mind, maxd, minm, maxm):
+    dc = ((data - mind) * (maxm-minm) / (maxd-mind) + mapmin)/mapmin
+    return dc
+def combine_calibrate(df):
+    xc = calibrate(df['Voltage'], minx, maxx, mapmin, mapmax)
+    yc = calibrate(df['Voltage_0'], miny, maxy, mapmin, mapmax)
+    zc = calibrate(df['Voltage_1'], minz, maxz, mapmin, mapmax)
+    dc = pd.DataFrame([xc,yc,zc]).T
+    dc.columns = ['x', 'y', 'z']
+    return dc
+
+def median_filter(data, f_size):
+	lgth, num_signal=data.shape
+	f_data=np.zeros([lgth, num_signal])
+	for i in range(num_signal):
+		f_data[:,i]=signal.medfilt(data.iloc[:,i], f_size)
+	return f_data
+def freq_filter(data, f_size, cutoff):
+	lgth, num_signal=data.shape
+	f_data=np.zeros([lgth, num_signal])
+	lpf=signal.firwin(f_size, cutoff, window='hamming')
+	for i in range(num_signal):
+		f_data[:,i]=signal.convolve(data.iloc[:,i], lpf, mode='same')
+	return f_data
+def plot_raw(data,fs):
+    num_rows, num_cols = data.shape
+    fig, ax=plt.subplots()
+    labels=['x','y','z']
+    color_map=['r', 'g', 'b']
+    index=np.arange(num_rows)/fs
+    for i in range(num_cols):
+        ax.plot(index, data.iloc[:,i], color_map[i], label=labels[i])
+    ax.set_xlim([0,num_rows/fs])
+    ax.set_xlabel('Time [sec]')
+    #ax.set_title('Time domain: '+title)
+    ax.legend()
+
+def center(data_filter):
+    x_mean, y_mean, z_mean = data_filter.mean()[0], data_filter.mean()[1], data_filter.mean()[2]
+    data_center = pd.DataFrame([data_filter[0]-x_mean, data_filter[1]-y_mean, data_filter[2]-z_mean])
+    return data_center.T
+
+def thresholding(data, datamin, datamax):
+    #datamin, datamax= data.min(), data.max()
+    data[(data >= datamin) & (data <= datamax)] = 0
+    return data
+def combine_threshold(data_center):
+    x = data_center[0].copy()
+    y = data_center[1].copy()
+    z = data_center[2].copy()
+    x = thresholding(x, xmin, xmax)
+    y = thresholding(y, ymin, ymax)
+    z = thresholding(z, zmin, zmax)
+    res = pd.DataFrame([x,y,z])
+    return res.T
+
+def plot_a(data, t):
+    #plt.figure()
+    data['t'] = t
+    data.columns = ['x', 'y', 'z', 't']
+    data['tt'] = data['t'].cumsum()
+    data.plot(x='tt',y=['x', 'y', 'z'])
+    plt.xlabel('time(s)')
+    plt.ylabel('acceleration(g)')
+
+def plot_v(data, t, yl):
+    #fig = plt.figure()
+    data = pd.DataFrame(data)
+    data['t'] = t
+    data.columns = ['v', 't']
+    data['tt'] = data['t'].cumsum()
+    data.plot(x='tt',y='v',legend=False)
+    plt.xlabel('time(s)')
+    plt.ylabel(yl)
+    #plt.show
+
+def plot_d(data, t, yl):
+    #fig = plt.figure()
+    data = pd.DataFrame(data)
+    data['t'] = t
+    data.columns = ['d', 't']
+    data['tt'] = data['t'].cumsum()
+    data.plot(x='tt',y='d',legend=False)
+    plt.xlabel('time(s)')
+    plt.ylabel(yl)
+
+# 讀取靜止加速度
+df = pd.read_csv(r".\data\30.csv")
+
+# 校正
+data = combine_calibrate(df)
+
+# 中值濾波 + 低通濾波
+median_data=median_filter(data, numtaps)
+lpf_data=freq_filter(data, numtaps, cutoff/fs)
+comb_data=freq_filter(pd.DataFrame(median_data), numtaps, cutoff/fs)
+plot_raw(pd.DataFrame(comb_data[100:-100]), fs)
+
+# 中心化
+data_filter = pd.DataFrame(comb_data[100:-100])
+data_center = center(data_filter)
+
+# 找門檻值
+
+x = data_center[0]
+y = data_center[1]
+z = data_center[2]
+#xmin, xmax = x.min(), x.max()
+#ymin, ymax = y.min(), y.max()
+#zmin, zmax = z.min(), z.max()
+#xmin, xmax = -0.02, 0.02
+#ymin, ymax = -0.02, 0.02
+#zmin, zmax = -0.02, 0.02
+xmin, xmax = -0, 0
+ymin, ymax = -0, 0
+zmin, zmax = -0, 0
+print(xmin, xmax)
+print(ymin, ymax)
+print(zmin, zmax)
+
+res = combine_threshold(data_center)
+plot_raw(pd.DataFrame(res), fs)
+
+def combine_all(path):
+    df = pd.read_csv(path)
+    data = combine_calibrate(df)
+    median_data = pd.DataFrame(median_filter(data, 5))
+    data_filter = pd.DataFrame(freq_filter(median_data, numtaps, cutoff/fs)[:])
+    data_center = center(data_filter)
+    res = pd.DataFrame(combine_threshold(data_center))
+    # 校正
+    plot_a(data, 0.1)
+
+    # 中值濾波
+    #plot_a(median_data, 0.1)
+
+    # 中值+低通濾波
+    plot_a(data_filter, 0.1)
+
+    # 中心化
+    plot_a(data_center, 0.1)
+    plt.ylim(-0.7,0.7)
+
+    # 門檻值
+    #plot_a(res, 0.1)
+    #plt.ylim(-0.7,0.7)
+    return res
+# 實測
+'''
+df = pd.read_csv(r".\data\put.csv")
+data = combine_calibrate(df)
+median_data = pd.DataFrame(median_filter(data, 5))
+data_filter = pd.DataFrame(freq_filter(median_data, numtaps, cutoff/fs)[:])
+data_center = center(data_filter)
+res = pd.DataFrame(combine_threshold(data_center))
+'''
+res_get = combine_all(r".\data\right2.csv")
+res_go = combine_all(r".\data\go.csv")
+res_put = combine_all(r".\data\put.csv")
+
+# 校正
+#plot_a(data, 0.1)
+
+# 中值濾波
+#plot_a(median_data, 0.1)
+
+# 中值+低通濾波
+#plot_a(data_filter, 0.1)
+
+# 中心化
+#plot_a(data_center, 0.1)
+#plt.ylim(-0.7,0.7)
+
+# 門檻值
+#plot_a(res, 0.1)
+#plt.ylim(-0.7,0.7)
+#res = pd.concat([res_get, res_go, res_put],axis=0)
+res_get.columns = ['x','y','z']
+res_go.columns = ['x','y','z']
+res_go['x'] = 0
+res_go['z'] = 0
+res_put.columns = ['x','y','z']
+res_put['x'] = -res_put['x']
+#print(res)
+# 積分運算
+freq1 = 0.0035 #for example freq=10 if you have 10 records per second
+freq2 = 0.11
+freq3 = 0.012
+# 9800 mm/s^2
+
+def v(res, freq):
+    return 9800*res["x"].cumsum() * freq, 9800*res["y"].cumsum() * freq, 9800*res["z"].cumsum() * freq
+
+def d(res, freq):
+    return ((9800*res["x"].cumsum() * freq).cumsum() * freq), ((9800*res["y"].cumsum() * freq).cumsum() * freq), ((9800*res["z"].cumsum() * freq).cumsum() * freq)
+# 一次積分速度
+v_x_get, v_y_get, v_z_get = v(res_get, freq1)
+v_x_go, v_y_go, v_z_go = v(res_go, freq2)
+v_x_put, v_y_put, v_z_put = v(res_put, freq3)
+
+plot_v(v_x_go, 0.1, 'Vx(mm/s)')
+plot_v(v_y_go, 0.1, 'Vy(mm/s)')
+plot_v(v_z_go, 0.1, 'Vz(mm/s)')
+
+
+# 二次積分位移
+d_x_get, d_y_get, d_z_get = d(res_get, freq1)
+d_x_go, d_y_go, d_z_go = d(res_go, freq2)
+d_x_put, d_y_put, d_z_put = d(res_put, freq3)
+plot_d(d_x_put, 0.1, 'Dx(mm)')
+plot_d(d_y_go, 0.1, 'Dy(mm)')
+plot_d(d_z_put, 0.1, 'Dz(mm)')
+
+# 3D位移
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot(pd.concat([d_x_get, d_x_go+d_x_get.iloc[-1], d_x_put+d_x_get.iloc[-1]+d_x_go.iloc[-1]], axis=0),
+        pd.concat([d_y_get, d_y_go+d_y_get.iloc[-1], d_y_put+d_y_get.iloc[-1]+d_y_go.iloc[-1]], axis=0),
+        pd.concat([d_z_get, d_z_go+d_z_get.iloc[-1], d_z_put+d_z_get.iloc[-1]+d_z_go.iloc[-1]], axis=0),)
+ax.set_xlabel("x(mm)")
+ax.set_ylabel("y(mm)")
+ax.set_zlabel("z(mm)")
+
+plt.show()
